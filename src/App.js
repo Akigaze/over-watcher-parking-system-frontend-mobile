2b1cfe5e02eff541af0de950b3ec467ea8322296
quinnHuang <quinn.huang@oocl.com>
2018-07-31 17:02:16 +0800

    [Quinn] init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginFrom from './components/LoginFrom.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <LoginFrom/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,32 @@
+import React,Component from 'react'
+import "antd-mobile/dist/antd-mobile.css"
+import { Button, List, InputItem, WhiteSpace, WingBlank,Icon} from "antd-mobile"
+
+export default class LoginFrom {
+    constructor(props) {
+        super(props)
+    }
+
+    render(){
+        return (
+            <div>
+                <WingBlank size="lg">
+                    <div>
+                        <WhiteSpace size="xl"/>
+                        <List>
+                            <InputItem placeholder="Your name">
+                                User
+                            </InputItem>
+                            <InputItem
+                                type="password" placeholder="Your password">
+                                Password
+                            </InputItem>
+                        </List>
+                        <WhiteSpace size="xl"/>
+                        <Button type="primary">Login</Button>
+                    </div>
+                </WingBlank>
+            </div>
+        )
+    }
+}
