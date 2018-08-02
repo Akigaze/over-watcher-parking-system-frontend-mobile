af649e6f6d4c25e60f6a67278eb3851352f05b19
TANGTR2 <tracy.tang@oocl.com>
2018-08-02 16:13:03 +0800

    [Tracy] alter NavBar to List in Scramble

--- src/components/Scramble .js
+++ src/components/Scramble .js
@@ -1,19 +1,15 @@
 import React,{Component} from "react";
-import { NavBar, Icon } from "antd-mobile"
+import { List } from "antd-mobile"
 
+const Item = List.Item;
 export default class Scramble extends Component{
     render(){
         return(
-    <div>
-        <NavBar
-            mode="light"
-            onLeftClick={() => console.log('onLeftClick')}
-            rightContent={[
-                <span  key="0">抢单</span> ,
-                <Icon  key="1" type="right" />]
-            }
-        >车牌号：粤00000</NavBar>
-    </div>
+            <div>
+                <List>
+                    <Item extra="抢单" arrow="horizontal" onClick={() => {}}>车牌号：粤00000</Item>
+                </List>
+            </div>
         );
     }
 }
