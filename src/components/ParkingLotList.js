a4f6f4aaea472ede1c6401527bf2de301c41559b
quinnHuang <quinn.huang@oocl.com>
2018-08-03 16:02:16 +0800

    [Quinn] select parking Lot to ready park

--- src/components/ParkingLotList.js
+++ src/components/ParkingLotList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { NavBar, List, Toast,Radio,Icon } from "antd-mobile";
+import { NavBar, List, Toast, Radio, Icon } from "antd-mobile";
 import Scramble from "./Scramble ";
 
 const Item = List.Item;
@@ -9,35 +9,48 @@ export default class ParkingLotList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedTab: "yellowTab"
+            parkingLotId: 0
         };
     }
+    select = (id,size) => {
+        if(size!==0){
+            this.setState({
+                parkingLotId: id
+            });
+        }else{
+            Toast.info('该停车场已经没有空位了 !!!', 1);
+        }
+
+    };
 
     render() {
-        const {history,location,match}=this.props.routerMatch
+        const { history, location, match } = this.props.routerMatch;
         console.log("停车场列表\n------------------------");
         console.log(match);
-        let parkingLotItems=this.props.parkingLots.map(lot=>{
-            const {id,name,size,initSize}=lot
+        let parkingLotItems = this.props.parkingLots.map(lot => {
+            let { id, name, size, initSize } = lot;
             return (
                 <RadioItem
-                key={id}
+                    key={id}
+                    checked={id == this.state.parkingLotId}
+                    onChange={() => {
+                        console.log("=======  " + id + "  ===========");
+                        this.select(id,size);
+                    }}
                 >
-                {name} ({size/initSize})
+                    {name} ( {size} / {initSize} )
                 </RadioItem>
-            )
-        })
+            );
+        });
         return (
             <div>
-            <NavBar
-                icon={<Icon type="left" />}
-                onLeftClick={() => history.push("/employees")}
-            >
-                停车地点
-            </NavBar>
-                <List>
-                    {parkingLotItems}
-                </List>
+                <NavBar
+                    icon={<Icon type="left" />}
+                    onLeftClick={() => history.push("/employees")}
+                >
+                    停车地点
+                </NavBar>
+                <List>{parkingLotItems}</List>
             </div>
         );
     }
