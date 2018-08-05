fd60aaf46beef1f955a93ed4e68667084fa02348
Eugene-Armstrong <1020454354@qq.com>
2018-08-06 02:40:55 +0800

    []

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -11,6 +11,7 @@ import {
 } from "antd-mobile";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import 'animate.css'
 import {LoginApi} from '../API/loginApi'
 
 export default class LoginForm extends Component {
@@ -61,30 +62,47 @@ export default class LoginForm extends Component {
             );
         }
         return (
-            <div>
+            <div className="animated shake">
+
+                <div style={{
+                    margin:"80px 0 20px 0",
+                    textAlign:"center",
+                    fontSize:"25px",
+                    lineHeight:"40px",
+                    color:"#26b5f8",
+                    fontFamily:"微软雅黑"
+                }}>
+                    欢迎登录&nbsp;
+                    <img
+                        style={{width:"30px",verticalAlign:"text-bottom"}}
+                        src="https://raw.githubusercontent.com/Eugene-Armstrong/my-online-pics/master/parking_mobile.png"
+                        alt="logo"
+                    />
+                    <br/>OverWatcher 停车场系统
+                </div>
                 <WingBlank size="lg">
-                    <div>
+                    <div style={{padding:"0 20px"}}>
                         <WhiteSpace size="xl" />
                         <List>
                             <InputItem
-                                placeholder="Your name"
+                                placeholder="请输入用户名"
                                 ref={this.userName}
                             >
-                                User
+                                用户名
                             </InputItem>
                             <InputItem
                                 type="password"
-                                placeholder="Your password"
+                                placeholder="请输入密码"
                                 ref={this.pwd}
                             >
-                                Password
+                                密&nbsp;&nbsp;&nbsp;码
                             </InputItem>
                         </List>
                         <WhiteSpace size="xs" />
                         {noticeBar}
                         <WhiteSpace size="md" />
                         <Button type="primary" onClick={this.login}>
-                            Login
+                            登录
                         </Button>
                     </div>
                 </WingBlank>
